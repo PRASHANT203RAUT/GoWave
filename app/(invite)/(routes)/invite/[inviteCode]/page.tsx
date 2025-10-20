247c2770dad59ceb927e591c7fdbb084a1297328
Prashant <prashant01968@gmail.com>
2025-10-21 00:16:10 +0530

    added some code

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -1,113 +1,61 @@
-// import { currentProfile } from "@/lib/current-profile";
-// import { db } from "@/lib/db";
-// import { RedirectToSignIn } from "@clerk/nextjs";
-// import { redirect } from "next/navigation";
-
-// interface InviteCodePageProps{
-//     params:{
-//         inviteCode:string;
-//     }
-// }
-
-
-// const InviteCodePage = async({params}:{
-//   params: { inviteCode: string };
-// }) => {
-
-//     const profile= await currentProfile()
-//     if(!profile){
-//         return RedirectToSignIn({})
-//     }
-//     if(!params.inviteCode){
-//         return redirect("/")
-//     }
-//     const existingServer=await db.server.findFirst({
-//         where:{
-//             inviteCode:params.inviteCode,
-//             members:{
-//                 some:{
-//                  profileId:profile.id
-//                     // <profileId:profile className="id"></profileId:profile>
-//                 }
-//             }
-//         }
-//     })
-//     if(existingServer){
-//         return redirect(`/servers/${existingServer.id}`);
-
-//     }
-    
-//     const server=await db.server.update({
-//         where:{
-//             inviteCode:params.inviteCode
-//         },
-//         data:{
-//             members:{
-//                 create:[
-//                     {
-//                         profileId:profile.id,
-//                     }
-//                 ]
-//             }
-//         }
-//     })
-
-// if(server){
-//     return redirect(`/servers/${server.id}`)
-// }
-
-//     return ( <div>hellow</div> );
-// }
- 
-// export default InviteCodePage;
-
-// ✅ CORRECT VERSION THAT BUILDS ON VERCEL
-
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { RedirectToSignIn } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-export default async function InviteCodePage({
-  params,
-}: {
-  params: { inviteCode: string };
-}) {
-  const profile = await currentProfile();
-
-  if (!profile) {
-    return RedirectToSignIn({});
-  }
-
-  const { inviteCode } = params;
-
-  if (!inviteCode) {
-    return redirect("/");
-  }
-
-  const existingServer = await db.server.findFirst({
-    where: {
-      inviteCode,
-      members: {
-        some: {
-          profileId: profile.id,
-        },
-      },
-    },
-  });
-
-  if (existingServer) {
-    return redirect(`/servers/${existingServer.id}`);
-  }
-
-  const server = await db.server.update({
-    where: { inviteCode },
-    data: {
-      members: {
-        create: [{ profileId: profile.id }],
-      },
-    },
-  });
-
-  return redirect(`/servers/${server.id}`);
+interface InviteCodePageProps{
+    params:{
+        inviteCode:string;
+    }
 }
+
+
+const InviteCodePage = async({params}:InviteCodePageProps) => {
+
+    const profile= await currentProfile()
+    if(!profile){
+        return <RedirectToSignIn />;
+
+    }
+    if(!params.inviteCode){
+        return redirect("/")
+    }
+    const existingServer=await db.server.findFirst({
+        where:{
+            inviteCode:params.inviteCode,
+            members:{
+                some:{
+                 profileId:profile.id
+                    // <profileId:profile className="id"></profileId:profile>
+                }
+            }
+        }
+    })
+    if(existingServer){
+        return redirect(`/servers/${existingServer.id}`);
+
+    }
+    
+    const server=await db.server.update({
+        where:{
+            inviteCode:params.inviteCode
+        },
+        data:{
+            members:{
+                create:[
+                    {
+                        profileId:profile.id,
+                    }
+                ]
+            }
+        }
+    })
+
+if(server){
+    return redirect(`/servers/${server.id}`)
+}
+
+    return ( <div>hellow</div> );
+}
+ 
+export default InviteCodePage;
